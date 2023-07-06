4cc71e5aacb0664036e2de00d78d95795f1e2562
BatanAndrei <yabatan777@gmail.com>
2023-07-06 10:41:17 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import back from './App.module.css';
+
+function App() {
+  
+  return (
+    <div className={back.position}>
+      <div className={back.context}></div>
+    </div>
+  );
+}
+
+export default App;
