93bd703cc3a77ab298914a4d48b18b834e46e792
BatanAndrei <yabatan777@gmail.com>
2023-07-07 21:39:03 +0300

    initial commit

--- src/ButtonGeo/ButtonGeo.jsx
+++ src/ButtonGeo/ButtonGeo.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import GeoButStyle from './ButtonGeo.module.css';
 
 const ButtonGeo = (props) => {
+    const [status, setStatus] = useState();
+    
+    const error = () => {
+        setStatus('Невозможно получить Ваше местоположение');
+    }
+
+     const success = (position) => {
+        setStatus(position.coords)
+          
+        console.log(position.coords)
+    }
+
+    const heandlerGeo = () => {
+        if (!navigator.geolocation) {
+            setStatus('Geolocation не поддерживается вашим браузером');
+        }else{
+            setStatus('Определение местоположения...');
+            navigator.geolocation.getCurrentPosition(success, error)
+        }
+    } 
 
     return (
         <>
-            <button className={GeoButStyle.button}>{props.nameButton}</button>
+            <button className={GeoButStyle.button} onClick={heandlerGeo}>{props.nameButton}</button>
         </>
     )
 }
--- src/ButtonWeather/ButtonWeather.jsx
+++ src/ButtonWeather/ButtonWeather.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { useState } from "react";
 import WeatherButStyle from './ButtonWea.module.css';
 
-
 const ButtonWeather = (props) => {
+    const [datas, setDatas] = useState();
+
+    const heandleWeahter = () => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => {setDatas(data)
+            console.log(data)
+        })
+    }
 
     return (
         <>
-            <button className={WeatherButStyle.button}>{props.nameButton}</button>
+            <button className={WeatherButStyle.button} onClick={heandleWeahter}>{props.nameButton}</button>
         </>
     )
 }
