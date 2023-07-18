964f0f031ecc0f7b0054c51e0b92df3669d32281
BatanAndrei <yabatan777@gmail.com>
2023-07-18 12:26:56 +0300

    initial commit

--- src/ButtonsReq/ButtonsReq.jsx
+++ src/ButtonsReq/ButtonsReq.jsx
@@ -15,7 +15,7 @@ const ButtonsReq = (props) => {
     const dataFromInput = props.dataInputBut;
     const requestTown = `${endPointWea}q=${dataFromInput}&limit=1&appid=1cc8827af65271374080f61bcb1007fe&lang=ru&units=metric`;
    
-
+   
     useEffect(() => {
         const heandleWeahter = (e) => {
             if(e.target === refGeo.current){
@@ -33,7 +33,7 @@ const ButtonsReq = (props) => {
                 .then(data => {setDatasWeaTown(data.name)
                     setDatasWeaCountryTemp(data.main.temp)
                     setDatasWeaDesc(data.weather[0].description)
-                /* console.log(data) */})
+                console.log()})
             }else if(e.target === refWea.current && !dataFromInput && e.target.tagName === 'SECTION'){
                 setStatusMess('↑ ↑ ↑ - Заполните поле - ↑ ↑ ↑');
                 refWea.current.style.fontWeight = 400;
