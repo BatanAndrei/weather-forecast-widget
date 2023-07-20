87db9693a7061a61860bfd1cffdb94587f5aea29
BatanAndrei <yabatan777@gmail.com>
2023-07-20 10:36:17 +0300

    initial commit

--- src/ThisDay/ThisDay.jsx
+++ src/ThisDay/ThisDay.jsx
@@ -3,8 +3,8 @@ import StyleDay from './ThisDay.module.css';
 import GlobalSvg from '../GlobalSvg/GlobalSvg';
 
 const ThisDay = ({ datasWeaTime, datasWeaTemp, datasWeaIcon, datasWeaCity }) => {
-     const temp = Math.round(datasWeaTemp);
-     const time = datasWeaTime;
+     const dataTemp = Math.round(datasWeaTemp);
+     const dataDateTime = datasWeaTime;
 
      const timeConverter = (UNIX_timestamp) => {
         let a = new Date(UNIX_timestamp * 1000);
@@ -15,18 +15,22 @@ const ThisDay = ({ datasWeaTime, datasWeaTemp, datasWeaIcon, datasWeaCity }) => 
         let hour = a.getHours();
         let min = a.getMinutes();
         let sec = a.getSeconds();
-        let time = date + '.' + month + ' ' + hour + ':' + min;
-        return time;
+
+        let valueDate = date + '.' + month;
+        let valueTime = hour + ':' + min;
+       
+            return [valueDate, valueTime];
     };
-    let res = timeConverter(time)
-     console.log(res)
-   
+    let valuesDateTime = timeConverter(dataDateTime)
+    let resultDate = valuesDateTime[0];
+    let resultTime = valuesDateTime[1]
+  
     return (
         <>
             <GlobalSvg icon={datasWeaIcon} />
-            <h4 className={StyleDay.temp}>{temp}°</h4>
-            <h4 className={StyleDay.day}>{res}</h4>
-            {/* <h4 className={StyleDay.time}></h4> */}
+            <h4 className={StyleDay.temp}>{dataTemp}°</h4>
+            <h4 className={StyleDay.day}>Сегодня: {resultDate}</h4>
+            <h4 className={StyleDay.time}>Время: {resultTime}</h4>
             <h4 className={StyleDay.city}>Город: {datasWeaCity}</h4>
         </>
     )
