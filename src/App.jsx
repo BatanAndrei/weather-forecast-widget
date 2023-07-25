c2a3bca6c56e5187a1e3e64f427b30b1f1cf17f9
BatanAndrei <yabatan777@gmail.com>
2023-07-25 13:04:46 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import back from './App.module.css';
 import InputTown from './Input/InputTown';
+import ButtonsReq from './ButtonsReq/ButtonsReq';
+import { useState } from 'react';
 
 
 function App() {
    
+    const [dataInput, setdataInput] = useState('');
+
+    const handleChange = (dataInput) => {
+        setdataInput(dataInput)
+    }
     return (
         <div className={back.position}>
             <div className={back.context}>
-                <InputTown />
+                <InputTown onChange={handleChange} />
                 <div className={back.displayWeather}></div>
+                <ButtonsReq dataInput={dataInput} />
             </div>
         </div>
     );
--- src/Input/InputTown.jsx
+++ src/Input/InputTown.jsx
@@ -1,22 +1,17 @@
 import React, { useState } from "react";
 import inputStyle from './Imput.module.css';
-import ButtonsReq from '../ButtonsReq/ButtonsReq';
 
 
-const InputTown = () => {
-    const [dataInput, setdataInput] = useState('');
+const InputTown = ({ onChange }) => {
     
-    function handleChange(e) {
-        setdataInput(e.target.value);
+    const handleChange = (e) => {
+        onChange(e.target.value)
           }
 
     return (
         <>
             <h1 className={inputStyle.text}>Прогноз погоды</h1>
-            <input type="text" className={inputStyle.modification} placeholder="Введите город или населённый пункт" onChange={handleChange} value={dataInput} />
-            <div>
-                <ButtonsReq dataInput={dataInput} />
-            </div>
+            <input type="text" className={inputStyle.modification} placeholder="Введите город или населённый пункт" onChange={handleChange} />
         </>
     )
 }
