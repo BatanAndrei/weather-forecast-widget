a3977f84e474bd287baa685a4318132ec62bf9be
BatanAndrei <yabatan777@gmail.com>
2023-07-25 13:32:05 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import back from './App.module.css';
-import InputTown from './Input/InputTown';
-import ButtonsReq from './ButtonsReq/ButtonsReq';
-import { useState } from 'react';
-
+import Form from './Form/Form';
 
 function App() {
-   
-    const [dataInput, setdataInput] = useState('');
-
-    const handleChange = (dataInput) => {
-        setdataInput(dataInput)
-    }
+  console.log('APP')
     return (
         <div className={back.position}>
             <div className={back.context}>
-                <InputTown onChange={handleChange} />
+                <Form />
                 <div className={back.displayWeather}></div>
-                <ButtonsReq dataInput={dataInput} />
             </div>
         </div>
     );
